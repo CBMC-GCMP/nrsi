807150eaad825214d48acb252f87a7eb193a32a3
Fabbiologia <favoretto.fabio@gmail.com>
2024-08-19 18:01:43 +0200

    final submission

--- 01-analysis.R
+++ 01-analysis.R
@@ -2,288 +2,215 @@ library(tidyverse)
 library(sf)
 library(patchwork)
 library(moments)
+library(ggridges)
+library(dafishr)
+
+resample_mean <- function(df, n = 9999) {
+  replicate(n, {
+    sampled <- df[sample(nrow(df), replace = TRUE), ]
+    mean((sampled$nrsi), na.rm = TRUE)
+  })
+}
+
 
 # Loading data ------------------------------------------------------------
-fish <- read_csv("data/rocky_reef_fish_database.csv")
+fish <- read_csv("data/fish_database_submission.csv") 
 
-nrsi <- fish |> 
+fish %>% 
+  mutate(name = paste0(region, "_", reef)) %>% 
+  group_by(protection) %>% 
+  summarise(n_distinct(reef))
+
+nrsi <-  fish %>% 
   mutate(index_levels = case_when(
-    str_detect(trophic_level_f, "2-2.5|4-4.5") ~ "ATL", 
+    str_detect(trophic_level_f, "4-4.5") ~ "UTL", 
+    str_detect(trophic_level_f, "2-2.5") ~ "LTL", 
     TRUE ~ "CTL"
   ), 
-  protection = factor(protection, levels = c("Not Protected", "Lightly Protected", "Fish Refuge", "Fully Protected"))) |>
-  mutate(degree = round(latitude)) |> 
-  group_by(year, id_reef, protection,degree, latitude, longitude, depth2, index_levels, transect) |> 
-  summarise(biomass = sum(biomass, na.rm = T)) |> 
-  group_by(year, id_reef, index_levels, degree, latitude, longitude, protection) |> 
-  summarise(biomass = mean(biomass, na.rm = T)) |> 
-  group_by(year, id_reef, degree, latitude, longitude, protection) |> 
-  mutate(rel_biomass = (biomass/sum(biomass, na.rm = T))*100) |> 
-  select(-biomass) %>% 
-  pivot_wider(names_from = index_levels, values_from = rel_biomass) |> 
-  mutate(nrsi = (ATL-CTL)/(ATL+CTL)) 
+  protection = factor(protection, levels = c("Not Protected", "Lightly Protected", "Fish Refuge", "Fully Protected"))) %>% 
+  mutate(degree = round(latitude)) %>% 
+  group_by(year, region, reef, protection, degree, latitude, longitude, depth2, index_levels, transect) %>% 
+  summarise(biomass = sum(biomass, na.rm = TRUE)) %>% 
+  group_by(year, region, reef, protection, index_levels, degree, latitude, longitude) %>% 
+  summarise(biomass = mean(biomass, na.rm = TRUE)) %>% 
+  group_by(year, region, reef, protection, degree, latitude, longitude) %>% 
+  mutate(rel_biomass = (biomass / sum(biomass, na.rm = TRUE)) * 100) %>% 
+  select(-biomass) %>%
+  pivot_wider(names_from = index_levels, values_from = rel_biomass) %>%
+  mutate(
+    UTL = if_else(is.na(UTL), 0, UTL),  # Replace NA with 0 for UTL calculations
+    LTL = if_else(is.na(LTL), 0, LTL),  # Replace NA with 0 for LTL calculations
+    CTL = if_else(is.na(CTL), 0, CTL),  # Replace NA with 0 for CTL calculations
+    nrsi = case_when(
+      LTL > UTL + CTL ~ UTL / (UTL + CTL),  # Condition 1: Use only UTL if LTL is greater than UTL + CTL
+      TRUE ~ (UTL + LTL - CTL) / (UTL + LTL + CTL)  # Condition 2: Compute as normal otherwise
+    )
+  ) 
 
 
-summary_stats <- nrsi %>%
-  group_by(protection) %>% 
-  summarise(
-    mean_nrsi = mean(nrsi, na.rm = TRUE),
-    sd_nrsi = sd(nrsi, na.rm = TRUE),
-    min_nrsi = min(nrsi, na.rm = TRUE),
-    max_nrsi = max(nrsi, na.rm = TRUE),
-    q1_nrsi = quantile(nrsi, 0.25, na.rm = TRUE),
-    median_nrsi = median(nrsi, na.rm = TRUE),
-    q3_nrsi = quantile(nrsi, 0.75, na.rm = TRUE), 
-    skewness_nrsi = skewness(nrsi, na.rm = TRUE),
-    kurtosis_nrsi = kurtosis(nrsi, na.rm = TRUE),
-    ci_lower = t.test(nrsi, conf.level = 0.95)$conf.int[1],
-    ci_upper = t.test(nrsi, conf.level = 0.95)$conf.int[2]
-  )
+reefs_monitored_long_term <- nrsi %>% 
+  ungroup() %>% 
+  select(year, reef) %>% 
+  unique() %>% 
+  group_by(reef) %>% 
+  summarise(series = n_distinct(year)) %>% 
+  filter(series > 19) %>% 
+  pull(reef)
 
-# Density plot ------------------------------------------------------------
+cp_sites <- fish %>% 
+  filter(region == "Cabo Pulmo") %>% 
+  filter(year < 2001) %>% 
+  pull(reef) %>% unique()
 
-nrsi  |> 
-  group_by(protection) |> 
-  mutate(median = median(nrsi)) |> 
-  ggplot(aes(x=nrsi, fill = protection))+
-  geom_density(alpha = 0.3) +
-  geom_vline(xintercept = 0) +
-  geom_vline(aes(xintercept = median), linetype = "dashed", col = 'red') +
-  xlim(c(-1,1)) +
-  labs(y="Values density", x = "NRSI", fill = "") +
-  scale_fill_manual(values = c("firebrick", "aquamarine", "lightgreen", "darkgreen")) +
-  facet_grid(protection~.,) +
+fishing_refuges <- c("REFUGIO_MORENA",
+                     "REFUGIO_SAN_MARCIAL",
+                     "SAN_DIEGO_ABNEGADO",
+                     "PUNTA_BOTELLA",
+                     "PARDITO_REFUGIO",
+                     "ISLOTE_AGUA_VERDE",
+                     "HABANA")
+
+### First is long term analysis with only sites consistently monitored
+ltms <- nrsi %>% 
+  filter(reef %in% c(reefs_monitored_long_term, cp_sites, fishing_refuges)) %>% 
+  mutate(protection = as.character(protection)) %>% 
+  mutate(protection = ifelse(is.na(protection), "Fishing Refuge", protection))
+
+
+resampled_prod <- ltms %>%
+  group_by(year, protection) %>%
+  nest() %>%
+  mutate(
+    ResampledMeans = map(data, ~resample_mean(.x)),
+    Median = map_dbl(ResampledMeans, median)
+  ) %>%
+  select(year, protection, ResampledMeans, Median)
+
+toplot_ltms <- resampled_prod %>%
+  unnest(ResampledMeans) 
+
+
+p1 <- toplot_ltms %>% 
+  mutate(protection = factor(protection, levels = rev(c("Not Protected", "Lightly Protected", "Fishing Refuge", "Fully Protected")))) %>% 
+  group_by(year, protection) %>% 
+  summarise(Median = mean(Median), min = min(ResampledMeans), max = max(ResampledMeans)) %>% 
+  ggplot(aes(x=year, y=Median)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = min, ymax = max), width=0.2) +
+  geom_hline(yintercept = 0) +
+  geom_smooth(se=F) +
+  scale_x_continuous(breaks = seq(1998, 2023, by=1)) +
+  ylim(-1, 1) +
+  labs(x = "Year", y = "NRSI Index") +
+  facet_grid(protection~.,)+
+  theme_bw() +
+  theme(strip.background = element_blank(), 
+        axis.text.x = element_text(angle = 90, vjust=.5),
+        legend.position = "")
+
+
+### Second is comparing protection levels from average of recent years (>2015)
+prot_levels <- nrsi %>% 
+  filter(year > 2008) %>% 
+  mutate(protection = as.character(protection)) %>% 
+  mutate(protection = ifelse(is.na(protection), "Fishing Refuge", protection))
+
+
+
+resampled_prod_prot_levels <- prot_levels %>%
+  group_by(region, protection) %>%
+  nest() %>%
+  mutate(
+    ResampledMeans = map(data, ~resample_mean(.x)),
+    Median = map_dbl(ResampledMeans, median)
+  ) %>%
+  select(region, protection, ResampledMeans, Median)
+
+toplot_prot_levels <- resampled_prod_prot_levels %>%
+  unnest(ResampledMeans) 
+
+
+resampled_prod_prot_levels <- prot_levels %>%
+  group_by(protection) %>%
+  nest() %>%
+  mutate(
+    ResampledMeans = map(data, ~resample_mean(.x)),
+    Median = map_dbl(ResampledMeans, median)
+  ) %>%
+  select( protection, ResampledMeans, Median)
+
+toplot_prot_levels <- resampled_prod_prot_levels %>%
+  unnest(ResampledMeans) 
+
+
+p2 <- toplot_prot_levels %>% 
+  mutate(protection = factor(protection, levels = (c("Not Protected", "Lightly Protected", "Fishing Refuge", "Fully Protected")))) %>% 
+  ggplot(aes(y = protection, x = ResampledMeans, fill = stat(x))) +
+  geom_density_ridges_gradient(scale = 1) +
+  geom_vline(xintercept = 0, linetype = 2) +
+  labs(x = "NRSI", y = "") +
+  scale_fill_gradientn(colors = c("firebrick", "orange", "gray", "green", "darkgreen"),
+                       values = scales::rescale(c(-1, -0.75, -0.5, 0.5, 0.75, 1)))+
+  xlim(-0.5,0.5) +
   theme_bw() +
   theme(legend.position = "", 
-        strip.background = element_blank(),
-        panel.spacing = unit(0, "lines"))
-  
-ggsave("figs/density_plot.png", dpi = 800, height = 5, width = 5)
-
-
-
-# BoxPlots -------------------------------------------------------------
-
-p1 <- nrsi %>%  
-  filter(protection !=  "Fish Refuge" | !year < 2006) %>% # eliminating unclear site
-  group_by(protection) %>% 
-  mutate(nrsi_avg = median(nrsi, na.rm = T)) %>% 
-  mutate(nrsiF = cut(nrsi, breaks=c(-1, -0.75, -0.25, 0, 0.25, 0.75, 1), include.lowest = TRUE))  %>% 
-  mutate(nrsiF = factor(nrsiF, levels = c('[-1,-0.75]', '(-0.75,-0.25]', '(-0.25,0]', '(0,0.25]', '(0.25,0.75]', '(0.75,1]'),
-                        labels = c("Very Low", "Low",  "Fair", "Fair",  "High", "Very high")))  %>% 
-  ggplot(aes(x=nrsi, y=protection)) +
-  geom_jitter(aes(col = nrsiF), height = .1, alpha = .4) +
-  geom_boxplot(outlier.colour = NA, width=.1, fatten =.1, alpha = 0.1) +
-  geom_point(aes(x=nrsi_avg), size =4) +
-  geom_vline(xintercept = 0, linetype = 2) +
-  scale_color_manual(values = c("firebrick", "orange", "gray", "green", "darkgreen"))+
-  labs(x="NRSI", y="", col = "") + 
-  xlim(-1,1) +
-  theme_bw() +
-  theme(strip.background = element_blank(), 
-        legend.position = "")
-p1
-
-
-ref_biomass <- fish |> 
-  mutate(protection = factor(protection, levels = c("Not Protected", "Lightly Protected", "Fish Refuge", "Fully Protected"))) |>
-  group_by(year, id_reef, protection, depth2, transect) |> 
-  summarise(biomass = sum(biomass, na.rm = T)) |> 
-  group_by(year, id_reef, protection) |> 
-  summarise(biomass = mean(biomass, na.rm = T)) %>% 
-  group_by(protection) %>% 
-  summarise(avg_biomass = log1p(median(biomass))) %>% 
-  filter(protection == "Not Protected") %>% pull(avg_biomass)
-
-p2 <- fish |> 
-  mutate(protection = factor(protection, levels = c("Not Protected", "Lightly Protected", "Fish Refuge", "Fully Protected"))) |>
-  group_by(year, id_reef, protection, depth2, transect) |> 
-  summarise(biomass = sum(biomass, na.rm = T)) |> 
-  group_by(year, id_reef, protection) |> 
-  summarise(biomass = mean(biomass, na.rm = T)) %>% 
-  ungroup() %>% 
-  mutate(avg_biomass = median(biomass)) %>% 
-  ggplot(aes(x=log1p(biomass), y=protection)) +
-  geom_jitter(height = .1, col = "gray60") +
-  geom_boxplot(outlier.colour = NA, width=.1, alpha = 0.1) +
-  geom_vline(xintercept = ref_biomass, linetype = 2) +
-  labs(x="log(Standing Biomass + 1)", y="", col = "") + 
-  theme_bw() +
-  theme(strip.background = element_blank(), 
-        axis.text.y = element_blank(),
-        legend.position = "top")
+        axis.text.x = element_text(angle = 90, vjust = .5))  
 p2
 
+### Third is showing results by reef on the map
+map_levels <- nrsi %>% 
+  filter(year > 2008) %>% 
+  mutate(protection = as.character(protection)) %>% 
+  mutate(protection = ifelse(is.na(protection), "Fishing Refuge", protection)) 
 
-# Time trends -------------------------------------------------------------
 
-p3 <- nrsi |>  
-  filter(protection !=  "Fish Refuge" | !year < 2006) |> # eliminating unclear site
-  mutate(nrsiF = cut(nrsi, breaks=c(-1, -0.75, -0.25, 0, 0.25, 0.75, 1), include.lowest = TRUE)) |> 
-  mutate(nrsiF = factor(nrsiF, levels = c('[-1,-0.75]', '(-0.75,-0.25]', '(-0.25,0]', '(0,0.25]', '(0.25,0.75]', '(0.75,1]'),
-                       labels = c("Very Low", "Low",  "Fair", "Fair",  "High", "Very high"))) |> 
-  ggplot(aes(x=year, y=nrsi, group=protection)) +
-  geom_point(aes(col = nrsiF), alpha = .4) +
-  geom_smooth(se = F, col = "black", method = "loess") +
-  geom_hline(yintercept = 0, linetype = 2) +
-  scale_color_manual(values = c("firebrick", "orange", "gray", "green", "darkgreen"))+
-  scale_x_continuous(breaks = seq(1998, 2022, by=1)) +
-  labs(x="Year", y="NRSI", col = "") + 
-  facet_grid(protection~.,) +
-  ylim(-1,1) +
+resampled_prod_map_levels <- map_levels %>%
+  group_by(region, reef, latitude, longitude) %>%
+  nest() %>%
+  mutate(
+    ResampledMeans = map(data, ~resample_mean(.x)),
+    Median = map_dbl(ResampledMeans, median)
+  ) %>%
+  select(region, reef, latitude, longitude, ResampledMeans, Median)
+
+toplot_map_levels <- resampled_prod_map_levels %>%
+  unnest(ResampledMeans) 
+
+
+p3 <- toplot_map_levels %>% 
+  group_by(region) %>% 
+  summarise(latitude = mean(latitude, na.rm=T), longitude = mean(longitude, na.rm=T), Median = mean(Median)) %>% 
+  st_as_sf(., coords = c("longitude", "latitude"), crs = 4326) %>% 
+  ggplot() +
+  geom_sf(data = dafishr::mx_shape, fill = "gray90") + 
+  geom_sf(aes(col = Median), size = 3) + 
+  scale_color_gradientn(colors = c("firebrick", "orange", "green", "darkgreen"),
+                       values = scales::rescale(c(-1, -0.5, 0, 0.5, 1)),
+                       limits = c(-1, 1)) +
+  coord_sf(xlim = c(-115, -95), ylim = c(15, 30)) +
+  labs(col = "NRSI Index") +
   theme_bw() +
-  theme(strip.background = element_blank(), 
-        axis.text.x = element_text(angle = 90),
-        strip.text = element_text(size = 6),
-        legend.position = "top")
+  theme(strip.background = element_blank(),
+        legend.title.position =  "top",
+        legend.title = element_text(hjust = 0.5),
+        legend.position = "bottom")
 p3
 
-p1 / p3 + plot_annotation(tag_levels = "A")
-
-ggsave("figs/Figure_3.jpeg", height = 8, width = 6, dpi = 800)
 
 
-nrsi_sf <- sf::st_as_sf(nrsi, coords = c("longitude", "latitude"), crs = 4326)
+library(patchwork)
+ 
 
-nrsi_sf |> 
-  group_by(id_reef, protection) |> 
-  summarise(nrsi = median(nrsi)) |> 
-  mutate(nrsiF = cut(nrsi, breaks=c(-1, -0.75, -0.25, 0, 0.25, 0.75, 1), include.lowest = TRUE)) |> 
-  mutate(nrsiF = factor(nrsiF, levels = c('[-1,-0.75]', '(-0.75,-0.25]', '(-0.25,0]', '(0,0.25]', '(0.25,0.75]', '(0.75,1]'),
-                        labels = c("Very Low", "Low",  "Fair", "Fair",  "High", "Very high"))) |> 
-  ggplot() +
-  geom_sf(data = dafishr::mx_shape) +
-  geom_sf(aes(col = (nrsiF))) +
-  coord_sf(xlim = c(min(nrsi$longitude)-1, max(nrsi$longitude)+1),
-           ylim = c(min(nrsi$latitude)-0.1, max(nrsi$latitude))+0.1) +
-  labs(col = "NRSI", subtitle = "All monitored sites in the program") +
-  scale_color_manual(values = c("firebrick", "orange", "gray", "green", "darkgreen"))+
-  ggspatial::annotation_scale(
-    location = "tr",
-    bar_cols = c("grey60", "white"),
-    text_family = "ArcherPro Book"
-  ) +
-  ggspatial::annotation_north_arrow(
-    location = "tr", which_north = "true",
-    pad_x = unit(0.4, "in"), pad_y = unit(0.4, "in"),
-    style = ggspatial::north_arrow_nautical(
-      fill = c("grey40", "white"),
-      line_col = "grey20",
-      text_family = "ArcherPro Book"
-    )) +
-  theme(legend.position = "right", 
-        legend.key = element_blank(),
-        panel.background = element_rect(fill = NA), 
-        panel.grid = element_line(color = "gray90", linetype = 2), 
-        panel.border = element_rect(fill = NA, color = "black")) 
+p3  
+ggsave("figs/figure_3_general_results.jpeg", height = 6, width = 6, dpi = 600)
 
-p2 <- nrsi_sf |> 
-  ggplot() +
-  geom_sf(data = dafishr::mx_shape) +
-  geom_sf(aes(col = protection)) +
-  scale_color_manual(values = c("firebrick", "aquamarine", "lightgreen", "darkgreen")) +
-  coord_sf(xlim = c(min(nrsi$longitude)-1, max(nrsi$longitude)+1),
-           ylim = c(min(nrsi$latitude)-0.1, max(nrsi$latitude))+0.1) +
-  labs(col = "", subtitle = "All monitored sites in the program") +
-  ggspatial::annotation_scale(
-    location = "tr",
-    bar_cols = c("grey60", "white"),
-    text_family = "ArcherPro Book"
-  ) +
-  ggspatial::annotation_north_arrow(
-    location = "tr", which_north = "true",
-    pad_x = unit(0.4, "in"), pad_y = unit(0.4, "in"),
-    style = ggspatial::north_arrow_nautical(
-      fill = c("grey40", "white"),
-      line_col = "grey20",
-      text_family = "ArcherPro Book"
-    )) +
-  scale_y_continuous(breaks = seq(15, 30, by=1)) +
-  theme(legend.position = "bottom", 
-        legend.text = element_text(size = 8),
-        legend.key = element_blank(),
-        panel.background = element_rect(fill = NA), 
-        panel.grid = element_line(color = "gray90", linetype = 2), 
-        panel.border = element_rect(fill = NA, color = "black")) 
+
 p2
-ggsave("figs/study_area.png", height = 6, width = 8, dpi = 800)
+ggsave("figs/figure_3_comparing.jpeg", height = 4, width = 4, dpi = 600)
 
 
+p1
+ggsave("figs/figure_3_time.jpeg", height = 5, width = 5, dpi = 600)
 
 
-
- # Statistical comparisons -------------------------------------------------
-
-library(betareg)
-
-# Assuming your data frame is named df and the index variable is called 'index_value'
-# Transform your index values from [-1, 1] to (0, 1)
-nrsi$transformed_index = (nrsi$nrsi + 1) / 2
-
-# Fit a beta regression model
-# Let's say you're modeling this transformed index as a function of some predictor 'predictor_var'
-beta_model = betareg(transformed_index ~ protection, data = nrsi)
-
-# Summary of the model
-summary(beta_model)
-
-# Assuming the coefficient for "Fully Protected"
-coef_fully_protected <- 0.77440
-
-# Convert the coefficient from log-odds to odds
-odds_increase <- exp(coef_fully_protected)
-
-# Calculate the percent increase from the odds
-percent_increase <- (odds_increase - 1) * 100
-
-# Print the percent increase
-print(percent_increase)
-
-nrsi %>% 
-  group_by(protection) %>% 
-  summarise(reef = n_distinct(id_reef))
-
-# Reef NRSI trends analysis -----------------------------------------------
-
-library(broom) # to change model results into data frame
-
-
-## dataset to model
-library(dplyr)
-library(betareg)
-library(broom)
-
-# Filter reefs with more than 3 years of data
-tomode <- nrsi |> 
-  group_by(id_reef) |> 
-  mutate(n_years = n_distinct(year)) |> # Getting the number of years
-  filter(n_years > 3)
-
-# Fit beta regression models for each reef and protection level, and extract slope of 'year'
-models <- tomode |> 
-  group_by(id_reef, protection) |> 
-  mutate(nrsi_transformed = (nrsi + 1) / 2) %>% 
-  do(tidy(betareg(nrsi_transformed ~ year, data = .))) |> 
-  filter(term == "year") |> 
-  mutate(trend = ifelse(estimate > 0, "Increasing", "Decreasing"))
-
-# Calculate the percentage of reefs with increasing or decreasing trends by protection level
-models_perc <- models |> 
-  group_by(protection) |> 
-  count(trend) |> 
-  mutate(perc = round((n/sum(n)) * 100))
-
-models_perc
-
-models_perc %>% 
-  mutate(perc = ifelse(trend == "Decreasing", perc * -1, perc)) %>% 
-  ggplot(aes(fill = trend, x = perc, y = protection)) +
-  geom_bar(position = "dodge", stat = "identity") +
-  labs(title = "Trend in Index by Protection Level",
-       y = "Protection Level",
-       x = "NRSI change over time (%)",
-       fill = "Trend") +
-  xlim(-100, 100) +
-  theme_bw() +
-  theme(legend.position = "")
 
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# nrsi
-The Normalized Reef Status Index allows to rapidly assess fishing pressure over fish community structure according to the disruption mechanisms of the human selectivity in fishery targets. 
+# Normalized Reef Status Index (NRSI)
+
+## Overview
+
+The Normalized Reef Status Index (NRSI) is a novel metric developed to assess the health of marine rocky reef ecosystems by analyzing the trophic structure of reef fish communities. This repository contains the code and data used to calculate the NRSI and apply it to reef sites across the Mexican Pacific, as detailed in our recent publication in *Ecological Indicators*.
+
+The NRSI is designed to be a rapid, scalable, and standardized tool for evaluating reef health. It enables comparisons across different spatial and temporal scales and can be used to assess the effectiveness of varying levels of marine protection.
+
+## Contents
+
+- **`data/`**: Contains the datasets used in the study, including fish biomass data, reef site locations, and protection levels.
+- **`01-analysis.R/`**: Includes R script used for calculating the NRSI.
+- **`figs/`**: Contains the figures generated for the publication.
+- **`supplementary_materials/`**: Includes additional materials referenced in the paper, such as supplementary tables and detailed methodological descriptions.
+
+## Installation
+
+To replicate the analysis and generate the figures, you will need to have R installed on your system. The `tidyverse` R package is required. You can install it by running the following command in your R console:
+
+```R
+install.packages(c("tidyverse"))
+```
+
+## Acknowledgments
+
+We acknowledge the contributions of the research teams and funding support from the International Community Foundation, the David and Lucile Packard Foundation, and National Geographic Pristine Seas. Special thanks to all collaborators who contributed to data collection and analysis over the years.
+
+## Contact
+
+For any questions or issues, please contact:
+
+- Fabio Favoretto (ffavoretto@ucsd.edu)
+
